59f0f151daddaa925ea44e33cfcc32043b015e7a
C <chkhk@foxmail.com>
2023-12-01 19:02:46 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <h3>
+    在文本框中输入内容，然后修改组件内容后回到浏览器，查看组件所在的 dom
+    结构，可以发现重复渲染
+  </h3>
+  <button @click="show = !show">{{ show ? 'hide' : 'show' }}</button>
+  <div id="app2" class="f-c">
+    <transition
+      enter-active-class="animate__animated animate__slideInLeft"
+      leave-active-class="animate__animated animate__fadeOutRight"
+    >
+      <component
+        v-if="show"
+        :key="Math.random()"
+        :is="HelloWorld"
+        @vue:mounted="childrenMounted"
+      />
+    </transition>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+const show = ref(true);
+
+function childrenMounted() {
+  console.log('childrenMounted');
+}
+</script>
+
+<style>
+button {
+  font-size: xx-large;
+}
+#app2 {
+  width: 100vw;
+  height: 50vh;
+  align-items: flex-start;
+  position: absolute;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+// animate.style
+import 'animate.css';
+
+import '@/assets/index.css';
+
+createApp(App).mount('#app');
